99f03aa0c1b6b51d2edf777399c8685892fbf86f
ben <benjaminwpeacock@gmail.com>
2013-11-06 21:55:11 -0600

    Matching program form tabs to document

--- html/inc/programform.inc.php
+++ html/inc/programform.inc.php
@@ -1,27 +1,35 @@
-<div class="container">
+<div class="container down40">
 	<form action="#" method="post"></form>
 		<!-- Nav tabs -->
 		<ul class="nav nav-tabs">
-		  <li><a href="#home" data-toggle="tab">Home</a></li>
-		  <li><a href="#profile" data-toggle="tab">Profile</a></li>
-		  <li><a href="#messages" data-toggle="tab">Messages</a></li>
-		  <li><a href="#settings" data-toggle="tab">Settings</a></li>
+		  <li><a href="#dates" data-toggle="tab">Dates</a></li>
+		  <li><a href="#message" data-toggle="tab">Message</a></li>
+		  <li><a href="#academics" data-toggle="tab">Academics</a></li>
+		  <li><a href="#life" data-toggle="tab">Life</a></li>
+		  <li><a href="#people" data-toggle="tab">People</a></li>
+		  <li><a href="#other" data-toggle="tab">other</a></li>
 		</ul>
 		<!-- Tab panes -->
 		<div class="tab-content">
-		  <div class="tab-pane" id="home">
+		  <div class="tab-pane" id="dates">
 		  	<button type="button" value="cancel">Cancel</button><button type="button" value="save">Save</button>
 		  	<label for="question1">Question 1</label>
 		  	<input type="text" name="question1" />
 		  	<button type="button" value="cancel">Cancel</button><button type="button" value="save">Save</button>
 		  </div>
-		  <div class="tab-pane" id="profile">
+		  <div class="tab-pane" id="message">
 		  	<p>foo</p>
 		  </div>
-		  <div class="tab-pane" id="messages">
+		  <div class="tab-pane" id="academics">
 		  	<p>bar</p>
 		  </div>
-		  <div class="tab-pane" id="settings">
+		  <div class="tab-pane" id="life">
+		  	<p>lorem</p>
+		  </div>
+		   <div class="tab-pane" id="people">
+		  	<p>lorem</p>
+		  </div>
+		   <div class="tab-pane" id="other">
 		  	<p>lorem</p>
 		  </div>
 		</div>
