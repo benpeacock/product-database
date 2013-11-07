76edcd566d501b6c2c5d2c04ac5e867a34ff6a36
ben <benjaminwpeacock@gmail.com>
2013-11-06 20:50:54 -0600

    Adding year select to dashboad, moving dashboard display into its own view

--- controllers/Dashboard.php
+++ controllers/Dashboard.php
@@ -1,9 +1,5 @@
 <?php
 require_once '../config.php';
-if (isset($_GET['action'])) {
-	$action = $_GET['action'];
-
-} //end GET actions
 
 if (isset($_POST)) {
 	$action = $_POST['action'];
@@ -11,20 +7,10 @@ if (isset($_POST)) {
 } // end POST actions
 
 include '../html/inc/header.inc.php';
-include '../html/inc/yearlist.inc.php';
-echo '<div class="container">';
-if (isset($_GET['id'])) {
-	$id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
-	if (filter_var($id, FILTER_VALIDATE_INT) == false) { exit('Invalid program id.'); }
-	$program_name = Program::getProgramObject($id);
-	echo '<h3>Editing: ' . $program_name->name;
+include '../html/views/dashboard.php';
+if (isset($program->id) && isset($year->year)) {
+	include '../html/inc/programform.inc.php';
 }
-if (isset($_GET['year'])) {
-	echo ', ' . $year['year'] . '</h3></div>';
-} else {
-	echo '</h3></div>';
-}
-include '../html/inc/programform.inc.php';
 include '../html/inc/footer.inc.php';
 
 
--- models/year.class.php
+++ models/year.class.php
@@ -19,4 +19,20 @@ class Year extends DatabaseObject {
 			echo 'Could not get year list' . $e->getMessage();
 		}
 	}
+	
+	public static function getyearObject($id) {
+		$dbh = Database::getPdo();
+		try {
+			$sql = "SELECT * FROM years WHERE id = :id";
+			$stmt = $dbh->prepare($sql);
+			$stmt->bindParam(':id', $id, PDO::PARAM_INT);
+			$stmt->execute();
+			$year = new Year();
+			$stmt->setFetchMode(PDO::FETCH_INTO, $year);
+			$result = $stmt->fetch();
+			return $result;
+		} catch (PDOException $e) {
+			echo 'Unable to get year object ' . $e->getMessage();
+		}
+	}
 }
