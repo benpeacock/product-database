c99f8684f00bdbe78fb03bca2420e0e609685f87
ben <benjaminwpeacock@gmail.com>
2013-11-12 19:20:46 -0600

    Formatting form fields

--- html/inc/programform.inc.php
+++ html/inc/programform.inc.php
@@ -15,25 +15,33 @@
 		<?php
 		foreach ($tabs as $tab) {
 					echo '<div class="tab-pane" id="' . $tab['name'] . '">';
-						echo '<div class="container form-group">';
+						echo '<div class="container">';
 							echo '<div class="row">';
 								echo '<button type="button" class="btn btn-default margin15" value="cancel">Cancel</button><button type="button" class="btn btn-primary" value="save">Save</button>';
 							echo '</div>';
-							$questions = Question::getQuestions($tab['id']);
- 							foreach ($questions as $question) {
- 								$answer = Answer::getAnswer($program->id, $year->id, $question['id']);
- 								echo '<label>' . $question['question'] . '</label>';
-								switch ($question['type']) {
-								case (1):
-									echo '<input type="text" name="' . $question['id'] . '" class="form-control" value="' . $answer->answer . '">';
-									break;
-								case (2):
-									echo '<textarea class="form-control" name="' . $question['id'] . '" value="' . $answer->answer . '"></textarea>';
-									break;
-								case (3):
-									echo '<input type="date" class="form-control" name="' . $question['id'] . '" value="' . $answer->answer . '" />';
-								}
- 							}							
+							echo '<div class="form-group">';
+								$questions = Question::getQuestions($tab['id']);
+	 							foreach ($questions as $question) {
+	 								$answer = Answer::getAnswer($program->id, $year->id, $question['id']);
+	 								switch ($question['type']) {
+									case (1):
+										echo '<label class="col-sm-3 control-label">' . $question['question'] . '</label>';
+										echo '<div class="col-sm-9 marginbottom20">';
+											echo '<input type="text" name="' . $question['id'] . '" class="form-control" value="' . $answer->answer . '">';
+										echo '</div>';
+										break;
+									case (2):
+										echo '<label>' . $question['question'] . '</label>';
+										echo '<textarea class="form-control" name="' . $question['id'] . '" value="' . $answer->answer . '"></textarea>';
+										break;
+									case (3):
+										echo '<label class="col-sm-3 control-label">' . $question['question'] . '</label>';
+										echo '<div class="col-sm-9 marginbottom20">';
+											echo '<input type="date" class="form-control" name="' . $question['id'] . '" value="' . $answer->answer . '" />';
+										echo '</div>';
+	 								}
+	 							}		
+	 						echo '</div>';					
 							echo '<div class="row">';
 								echo '<button type="button" class="btn btn-default margin15" value="cancel">Cancel</button><button type="button" class="btn btn-primary" value="save">Save</button>';
 							echo '</div>';
