fab105862a608d595050c355bca1d2a2aa2ddfdc
ben <benjaminwpeacock@gmail.com>
2013-11-13 15:53:27 -0600

    adding toggle of programform display based on edit/delete button in nav menu

--- html/inc/programform.inc.php
+++ html/inc/programform.inc.php
@@ -13,40 +13,11 @@
 		</ul>
 		<div class="tab-content">
 		<?php
-		foreach ($tabs as $tab) {
-					echo '<div class="tab-pane" id="' . $tab['name'] . '">';
-						echo '<div class="container">';
-							echo '<div class="row">';
-								echo '<button type="button" class="btn btn-default margin15" value="cancel">Cancel</button><button type="button" class="btn btn-primary" value="save">Save</button>';
-							echo '</div>';
-							echo '<div class="form-group">';
-								$questions = Question::getQuestions($tab['id']);
-	 							foreach ($questions as $question) {
-	 								$answer = Answer::getAnswer($program->id, $year->id, $question['id']);
-	 								switch ($question['type']) {
-									case (1):
-										echo '<label class="col-sm-3 control-label">' . $question['question'] . '</label>';
-										echo '<div class="col-sm-9 marginbottom20">';
-											echo '<input type="text" name="' . $question['id'] . '" class="form-control" value="' . $answer->answer . '">';
-										echo '</div>';
-										break;
-									case (2):
-										echo '<label>' . $question['question'] . '</label>';
-										echo '<textarea class="form-control" name="' . $question['id'] . '" value="' . $answer->answer . '"></textarea>';
-										break;
-									case (3):
-										echo '<label class="col-sm-3 control-label">' . $question['question'] . '</label>';
-										echo '<div class="col-sm-9 marginbottom20">';
-											echo '<input type="date" class="form-control" name="' . $question['id'] . '" value="' . $answer->answer . '" />';
-										echo '</div>';
-	 								}
-	 							}		
-	 						echo '</div>';					
-							echo '<div class="row">';
-								echo '<button type="button" class="btn btn-default margin15" value="cancel">Cancel</button><button type="button" class="btn btn-primary" value="save">Save</button>';
-							echo '</div>';
-						echo '</div>';
-					echo '</div>';
+		if (isset($_GET['action']) && $_GET['action'] == 'edit') {
+			
+			include 'program_edit.inc.php';
+		} else {
+		echo 'flurm';
 		}
 		?>
 		</div>
